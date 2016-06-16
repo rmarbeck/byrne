0a56d30d9456d428a37241ff5c3334f7688a09f2
Raphaël Marbeck <raphael@hometime.fr>
2016-06-16 14:20:44 +0200

    Missing file

---
+++ app/utils/ItemsHelper.java
@@ -0,0 +1,74 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import play.data.Form;
+import models.Invoice;
+import models.InvoiceItem;
+
+public class ItemsHelper {
+	public static final String ITEMS_FIELD_NAME = "items";
+	public static final String ITEMS_NAME_FIELD_NAME = "name";
+	public static final String ITEMS_COST_FIELD_NAME = "cost";
+	public static final String ITEMS_NO_VAT_FIELD_NAME = "noVat";
+	
+	public static List<InvoiceItem> getItems(Form<Invoice> form) {
+		if (form.hasErrors()) {
+			return extractInfoFromFormInError(form);
+		} else {
+			return form.get().items;
+		}
+	}
+	
+	private static List<InvoiceItem> extractInfoFromFormInError(Form<Invoice> form) {
+		List<InvoiceItem> items = new ArrayList<InvoiceItem>();
+		Map<String, String> datas = form.data();
+		int index = 0;
+		while(true) {
+			if(datas.containsKey(getKeyNameByIndexForName(index))) {
+				items.add(new InvoiceItem(	getValueByIndexForName(datas, index),
+											getValueByIndexForCost(datas, index),
+											getValueByIndexForNoVat(datas, index)));
+			} else {
+				break;
+			}
+			index++;
+		}
+		return items;
+	}
+	
+	private static String getValueByIndexForName(Map<String, String> datas, int index) {
+		return datas.get(getKeyNameByIndexForName(index));
+	}
+	
+	private static Float getValueByIndexForCost(Map<String, String> datas, int index) {
+		return Float.parseFloat(datas.get(getKeyNameByIndexForCost(index)));
+	}
+	
+	private static boolean getValueByIndexForNoVat(Map<String, String> datas, int index) {
+		return Boolean.getBoolean(datas.get(getKeyNameByIndexForNoVat(index)));
+	}
+	
+	private static String getKeyNameByIndexForName(int index) {
+		return getKeyNameByForGivenValue(index,ITEMS_NAME_FIELD_NAME);
+	}
+	
+	private static String getKeyNameByIndexForCost(int index) {
+		return getKeyNameByForGivenValue(index,ITEMS_COST_FIELD_NAME);
+	}
+	
+	private static String getKeyNameByIndexForNoVat(int index) {
+		return getKeyNameByForGivenValue(index,ITEMS_NO_VAT_FIELD_NAME);
+	}
+	
+	private static String getKeyNameByForGivenValue(int index, String valueName) {
+		return getKeyNameByIndex(index)+"."+valueName;
+	}
+	
+	private static String getKeyNameByIndex(int index) {
+		return ITEMS_FIELD_NAME+"["+index+"]";
+	}
+
+}
