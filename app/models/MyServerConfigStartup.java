e43a9fb1d4debd1be3c8e3c3b3f99760df742837
Raphaël Marbeck <raphael@hometime.fr>
2016-06-16 11:48:04 +0200

    Messages

--- app/models/MyServerConfigStartup.java
+++ app/models/MyServerConfigStartup.java
@@ -5,6 +5,8 @@ import com.avaje.ebean.config.dbplatform.PostgresPlatform;
 import com.avaje.ebean.config.dbplatform.DbIdentity;
 import com.avaje.ebean.config.ServerConfig;
 import com.avaje.ebean.event.ServerConfigStartup;
+import com.typesafe.config.ConfigFactory;
+
 
 // This is needed to change the way Postgres handles
 // id generation
@@ -12,11 +14,13 @@ import com.avaje.ebean.event.ServerConfigStartup;
 public class MyServerConfigStartup implements ServerConfigStartup {
      @Override
      public void onStart(ServerConfig serverConfig) {
-         PostgresPlatform postgresPlatform = new PostgresPlatform();
-         DbIdentity dbIdentity = postgresPlatform.getDbIdentity();
-         dbIdentity.setSupportsGetGeneratedKeys(false);
-         dbIdentity.setSupportsSequence(true);
-         dbIdentity.setIdType(IdType.GENERATOR);
-         serverConfig.setDatabasePlatform(postgresPlatform);
+    	 if (ConfigFactory.load().getBoolean("change.serial.generator.for.postgre")) {
+	         PostgresPlatform postgresPlatform = new PostgresPlatform();
+	         DbIdentity dbIdentity = postgresPlatform.getDbIdentity();
+	         dbIdentity.setSupportsGetGeneratedKeys(false);
+	         dbIdentity.setSupportsSequence(true);
+	         dbIdentity.setIdType(IdType.GENERATOR);
+	         serverConfig.setDatabasePlatform(postgresPlatform);
+    	 }
      }
 }
